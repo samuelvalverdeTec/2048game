f745eef46962fb4c7165e9512d8a8fe0770e4c2e
samuelvalverdee <savalverde@estudiantec.cr>
2023-07-31 17:00:04 -0600

    .

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 class Game {
 
     size;   // 4x4
+    level;  // 0, 1 o 2 (facil, medio, dificil)
     board;
     final;
 
@@ -9,8 +10,22 @@ class Game {
         this.size = 4;
         this.board = Array(size).fill( Array(size).fill("") )
         this.final = false;
+        //this.level = document.getElementsByClassName("radiobutton").
+        /*if(this.level == null){
+
+        } else {
+            document.getElementById("header_play-pause").addEventListener('click', start());
+        }*/
 
     }
-}
 
-const game = new Game();
+    start() {
+
+        const game = new Game();
+        game.random();
+
+    }
+
+    random
+
+}
