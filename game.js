2c945fff5fe4c7becdce552537e4d12ca00faf2d
Erick25Kauffmann <erikaupor@gmail.com>
2023-08-10 01:27:21 -0600

    crear ficha (no sirve)

--- game.js
+++ game.js
@@ -2,11 +2,26 @@ class Ficha {
 
     numero;
     valores = [2,4];
+    posX;
+    posY;
 
     constructor() {
 
         const index = Math.random();    // 0 o 1
         this.numero = valores[index];   // 2 o 4
+        this.posY = 0;
+        posXtemp = [0,0];
+        const index2 = Math.random();
+        const index3 = Math.random();
+        posX1 = [1,2];
+        posX2 = [3,4];
+        posX1temp = posX1[index2];
+        posX2temp = posX2[index3];
+        posXtemp[0] = posX1temp;
+        posXtemp[1] = posX2temp;
+        const index4 = Math.random();
+        this.posX = posXtemp[index4];
+
 
     }
 
@@ -46,9 +61,34 @@ class Game {
         const game = new Game();
         game.random();
 
+        crearFicha();
+
     }
 
 }
+
+
+function crearFicha(){
+    fichaAct = new Ficha();
+    if(fichaAct.posX == 1){
+        casilla = document.getElementById(F0C1);
+        casilla.style.backgroundColor = 'red';
+    }
+    else if(fichaAct.posX == 2){
+        casilla = document.getElementById(F0C2);
+        casilla.style.backgroundColor = 'red';
+    }
+    else if(fichaAct.posX == 3){
+        casilla = document.getElementById(F0C3);
+        casilla.style.backgroundColor = 'red';
+    }
+    else {
+        casilla = document.getElementById(F0C4);
+        casilla.style.backgroundColor = 'red';
+    }
+
+}
+
 
 function keyPressed() {
     if (!gameOver && !gameWon) {
